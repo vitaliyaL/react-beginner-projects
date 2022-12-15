0cce3e1972a2e9bf11801a86f2dfe610be17e9bc
vitaliyaL <vita0623@mail.ru>
2022-12-15 15:03:25 +0300

    feat: working counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './index.scss';
+import React from "react"
 
 function App() {
+const [count,setCount]=useState(0);
+const handleCountPlus=()=>{
+setCount(count+1)
+}
+const handleCountMinus=()=>{
+  setCount(count-1)
+}
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={handleCountMinus}>- Минус</button>
+        <button className="plus" onClick={handleCountPlus}>Плюс +</button>
       </div>
     </div>
   );
